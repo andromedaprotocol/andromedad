d6e8e89cc96d1b9c8699dfcc6097d4089e2b392f
boly1028 <johnrafman1028@gmail.com>
2024-01-24 08:48:26 -0600

    fix: change expected_keepers

--- x/nibftdfvas/types/expected_keepers.go
+++ x/nibftdfvas/types/expected_keepers.go
@@ -1,25 +1,21 @@
 package types
 
 import (
+	math "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-
-
-
-
-
-
-type ParamsKeeper interface {
-	// Methods imported from params should be defined here
+type StakingKeeper interface {
+	// Methods imported from staking should be defined here
+	GetBondedValidatorsByPower(ctx sdk.Context) []stakingtypes.Validator
+	StakingTokenSupply(ctx sdk.Context) math.Int
 }
-
-
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
 type AccountKeeper interface {
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
 	// Methods imported from account should be defined here
 }
 
@@ -27,4 +23,13 @@ type AccountKeeper interface {
 type BankKeeper interface {
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
+	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+  SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+}
+
+// ParamSubspace defines the expected Subspace interface for parameters.
+type ParamSubspace interface {
+	Get(sdk.Context, []byte, interface{})
+	Set(sdk.Context, []byte, interface{})
 }
