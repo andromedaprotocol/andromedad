936480fbada06408af281dc73d7a066e7a4afc81
boly1028 <johnrafman1028@gmail.com>
2024-01-24 08:50:37 -0600

    fix: update genesis state

--- x/nibftdfvas/types/params.go
+++ x/nibftdfvas/types/params.go
@@ -1,15 +1,11 @@
 package types
 
 import (
-	
-
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	"gopkg.in/yaml.v2"
+	// "gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
-
-
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -17,22 +13,18 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams(
-) Params {
-	return Params{
-	}
+func NewParams() Params {
+	return Params{}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams(
-	)
+	return NewParams()
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{
-	}
+	return paramtypes.ParamSetPairs{}
 }
 
 // Validate validates the set of params
@@ -41,7 +33,7 @@ func (p Params) Validate() error {
 }
 
 // String implements the Stringer interface.
-func (p Params) String() string {
-	out, _ := yaml.Marshal(p)
-	return string(out)
-}
+// func (p Params) String() string {
+// 	out, _ := yaml.Marshal(p)
+// 	return string(out)
+// }
