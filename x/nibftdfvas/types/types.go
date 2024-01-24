d5258d5040d82308aa8c5cba500d0e9b7b6c17cc
boly1028 <johnrafman1028@gmail.com>
2024-01-24 08:51:50 -0600

    fix: disable types's func

--- x/nibftdfvas/types/types.go
+++ x/nibftdfvas/types/types.go
@@ -1,73 +1,73 @@
 package types
 
-import (
-	"github.com/cosmos/cosmos-sdk/types"
-)
+// import (
+// 	"github.com/cosmos/cosmos-sdk/types"
+// )
 
-type DAOParams struct {
-	TokenOutflowPerBlock     int    `json:"token_outflow_per_block"`
-	DirectToValidatorPercent int    `json:"direct_to_validator_percent"`
-}
+// type DAOParams struct {
+// 	TokenOutflowPerBlock     int    `json:"token_outflow_per_block"`
+// 	DirectToValidatorPercent int    `json:"direct_to_validator_percent"`
+// }
 
-type GenesisState struct {
-	DAOParams DAOParams `json:"dao_params"`
-	// Add other genesis state as needed
-}
+// type GenesisState struct {
+// 	DAOParams DAOParams `json:"dao_params"`
+// 	// Add other genesis state as needed
+// }
 
-type MyModuleAccount struct {
-	Address string `json:"address"`
-	Balance int    `json:"balance"`
-}
+// type MyModuleAccount struct {
+// 	Address string `json:"address"`
+// 	Balance int    `json:"balance"`
+// }
 
-type MsgTokenDistribution struct {
-	Sender string `json:"sender"`
-	Amount int    `json:"amount"`
-}
+// type MsgTokenDistribution struct {
+// 	Sender string `json:"sender"`
+// 	Amount int    `json:"amount"`
+// }
 
-var _ types.Msg = &MsgTokenDistribution{}
+// var _ types.Msg = &MsgTokenDistribution{}
 
-func NewMsgTokenDistribution(sender string, amount int) MsgTokenDistribution {
-	return MsgTokenDistribution{
-		Sender: sender,
-		Amount: amount,
-	}
-}
+// func NewMsgTokenDistribution(sender string, amount int) MsgTokenDistribution {
+// 	return MsgTokenDistribution{
+// 		Sender: sender,
+// 		Amount: amount,
+// 	}
+// }
 
-func (msg MsgTokenDistribution) Route() string {
-	return "mymodule"
-}
+// func (msg MsgTokenDistribution) Route() string {
+// 	return "mymodule"
+// }
 
-func (msg MsgTokenDistribution) Type() string {
-	return "token_distribution"
-}
+// func (msg MsgTokenDistribution) Type() string {
+// 	return "token_distribution"
+// }
 
-func (msg MsgTokenDistribution) ValidateBasic() error {
-	// Add validation logic if needed
-	return nil
-}
+// func (msg MsgTokenDistribution) ValidateBasic() error {
+// 	// Add validation logic if needed
+// 	return nil
+// }
 
-func (msg MsgTokenDistribution) GetSignBytes() []byte {
-	return types.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
-}
+// func (msg MsgTokenDistribution) GetSignBytes() []byte {
+// 	return types.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+// }
 
-func (msg MsgTokenDistribution) GetSigners() []types.AccAddress {
-	sender, err := types.AccAddressFromBech32(msg.Sender)
-	if err != nil {
-		panic(err)
-	}
-	return []types.AccAddress{sender}
-}
+// func (msg MsgTokenDistribution) GetSigners() []types.AccAddress {
+// 	sender, err := types.AccAddressFromBech32(msg.Sender)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return []types.AccAddress{sender}
+// }
 
-func (msg MsgTokenDistribution) ValidateBasic() error {
-	if msg.Amount <= 0 {
-		return errors.New("amount should be greater than 0")
-	}
+// func (msg MsgTokenDistribution) ValidateBasic() error {
+// 	if msg.Amount <= 0 {
+// 		return errors.New("amount should be greater than 0")
+// 	}
 
-	if _, err := types.AccAddressFromBech32(msg.Sender); err != nil{
-		return errors.New("invalid sender address")
-	}
+// 	if _, err := types.AccAddressFromBech32(msg.Sender); err != nil{
+// 		return errors.New("invalid sender address")
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 
